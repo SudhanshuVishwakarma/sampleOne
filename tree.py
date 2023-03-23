c458f610327291520a51f1ec3a9476112a9c2f7d
SudhanshuVishwakarma <vsudhanshu18@gmail.com>
2023-03-23 15:40:45 +0530

    tree

--- tree.py
+++ tree.py
@@ -51,7 +51,7 @@ class BST:
             self.rchild.In_order()
 
     # delection
-    def delete(self, data):
+    def delete(self, data, curr):
         # if tree is mpt
         if self.key is None:
             print("Tree is empty")
@@ -59,30 +59,54 @@ class BST:
         # find the node
         if data < self.key:
             if self.lchild:
-                self.lchild = self.lchild.delete(data)
+                self.lchild = self.lchild.delete(data, curr)
             else:
                 print("Given is not present in the tree")
         elif data > self.key:
             if self.rchild:
-                self.rchild = self.rchild.delete(data)
+                self.rchild = self.rchild.delete(data, curr)
             else:
                 print("Given is not present in the tree")
         else:
             # caese := 0 child ; 1 child ; 2 child
             if self.lchild is None:
                 temp = self.rchild
+                if data == curr:
+                    self.key = temp.key
+                    self.lchild = temp.lchild
+                    self.rchild = temp.rchild
+                    temp = None
+                    return
                 self = temp
-                return
+                return temp
             if self.rchild is None:
                 temp = self.lchild
+                if data == curr:
+                    self.key = temp.key
+                    self.lchild = temp.lchild
+                    self.rchild = temp.rchild
+                    temp = None
+                    return
                 self = temp
                 return
             node = self.rchild
             while node.lchild:
                 node = node.lchild
             self.key = node.key
-            self.rchild = self.rchild.delete(node.key)
+            self.rchild = self.rchild.delete(node.key, curr)
         return self
+
+    def Min_key(self):
+        current = self
+        while current.lchild:
+            current = current.lchild
+        print("Node with the smallest key", current.key)
+
+    def Max_key(self):
+        current = self
+        while current.rchild:
+            current = current.rchild
+        print("Node with the maximum key", current.key)
 
     # seraching
     def search(self, data):
@@ -102,14 +126,29 @@ class BST:
                 print("Node is not present in tree")
 
 
+# count
+def count(node):
+    if node is None:
+        return 0
+    return 1 + count(node.lchild) + count(node.rchild)
+
+
 root = BST(30)
 list1 = [20, 4, 30, 56, 4, 1, 5, 6]
 for i in list1:
     root.insert(i)
-root.search(6)
+# root.search(6)
 
-root.delete(60)
-print("Node is gone")
+print("Number of nodes", count(root))
+
+# if count(root) > 1:
+#     root.delete(30, root.key)
+# else:
+#     print("can not perform delete opearation")
+
+root.Min_key()
+root.Max_key()
+
 print(" Pre-order:")
 root.pre_order()
 print("\n Post-order:")
@@ -130,3 +169,10 @@ root.In_order()
 # print(root.rchild.key)
 # print(root.rchild.lchild)
 # print(root.rchild.rchild)
+
+# import heapq
+
+# list1 = [(11, "Riya"), (2, "siya"), (3, "giya")]
+# heapq.heapify(list1)
+# for i in range(len(list1)):
+#     print(heapq.heappop(list1))
