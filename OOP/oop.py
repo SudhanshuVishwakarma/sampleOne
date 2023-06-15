284cd892be3a1e622554cdf96648a956c587d9f2
SudhanshuVishwakarma <vsudhanshu18@gmail.com>
2023-06-16 00:22:02 +0530

    Opps

--- OOP/oop.py
+++ OOP/oop.py
@@ -1,2 +1,15 @@
-x = 1
-print(type(x))
+class Dog:
+    def __init__(self, name) -> None:
+        self.name = name
+        print(name)
+
+    def add(self, x):
+        return x + 1
+
+    def bark(self):
+        print("bark")
+
+
+d = Dog("Muse")
+# print(d.add(3))
+# d.bark()
