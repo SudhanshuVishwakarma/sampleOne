a7c422c073287ad5a053b58453753071d67de4a6
SudhanshuVishwakarma <vsudhanshu18@gmail.com>
2023-06-17 23:22:37 +0530

    afd

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -6,12 +6,30 @@ class pet:
     def show(self):
         print(f"I am {self.name}and I am {self.age} yr old")
 
+    def speak(self):
+        print("IDK")
 
-class cat:
+
+class cat(pet):
     def speak(self):
         print("meow")
 
 
-class Dog:
+class Dog(pet):
     def speak(self):
         print("Bark")
+
+
+class fish(pet):
+    pass
+
+
+p = pet("Tim", 12)
+p.show()
+c = cat("Bill", 34)
+c.show()
+d = Dog("SAGE MOM", 23)
+d.show()
+d.speak()
+f = fish("lol", 33)
+f.speak()
